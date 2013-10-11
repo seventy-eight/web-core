a61aa4ce276e584f47660dcaa3a96822ea2556e9
Christian Wolfgang <wolfgarnet@gmail.com>
2013-10-12 00:33:52 +0200

    [MARKUP-2] First draft of generator interface

---
+++ utilities/src/main/java/org/seventyeight/markup/Generator.java
@@ -0,0 +1,19 @@
+package org.seventyeight.markup;
+
+/**
+ * @author cwolfgang
+ */
+public interface Generator {
+    public void section( StringBuilder output, int count );
+    public void subSection( StringBuilder output, int count );
+
+    public void italic( StringBuilder output, int count );
+    public void bold( StringBuilder output, int count );
+    public void italicAndBold( StringBuilder output, int count );
+
+    public void unorderedList( StringBuilder output, Parser.MarkUp markUp, int consumed );
+
+    public void onEmptyLine( StringBuilder output );
+
+    public Parser.MarkUp getLastMarkUp();
+}
