fcaf43c5bb4757f66d8d022dfb675cae9eb7b115
Christian Wolfgang <wolfgarnet@gmail.com>
2013-10-12 00:34:36 +0200

    [MARKUP-3] Initial implementation of simple parser

---
+++ utilities/src/main/java/org/seventyeight/markup/SimpleParser.java
@@ -0,0 +1,21 @@
+package org.seventyeight.markup;
+
+/**
+ * @author cwolfgang
+ */
+public class SimpleParser extends Parser {
+
+
+    public SimpleParser( Generator generator ) {
+        super( generator );
+
+        markUps.add( new MarkUp( new char[]{'='}, MarkUpType.section ) );
+        markUps.add( new MarkUp( new char[]{'=', '='}, MarkUpType.subSection ) );
+
+        markUps.add( new MarkUp( new char[]{'\'', '\''}, MarkUpType.italic ) );
+        markUps.add( new MarkUp( new char[]{'\'', '\'', '\''}, MarkUpType.bold ) );
+        markUps.add( new MarkUp( new char[]{'\'', '\'', '\'', '\'', '\''}, MarkUpType.italicAndBold ) );
+
+        markUps.add( new MarkUp( new char[]{'*'}, MarkUpType.unorderedList, true ) );
+    }
+}
