d43032853dd09a13a32f733a1be063d656da49c2
Christian Wolfgang <wolfgarnet@gmail.com>
2013-11-09 23:46:48 +0100

    [PROJ-3] Added project

--- project/src/main/java/org/seventyeight/web/project/model/Project.java
+++ project/src/main/java/org/seventyeight/web/project/model/Project.java
@@ -1,7 +1,51 @@
 package org.seventyeight.web.project.model;
 
+import org.seventyeight.database.mongodb.MongoDocument;
+import org.seventyeight.web.model.*;
+
 /**
  * @author cwolfgang
  */
-public class Project {
+public class Project extends Resource<Project> {
+
+    public Project( Node parent, MongoDocument document ) {
+        super( parent, document );
+    }
+
+    @Override
+    public Saver getSaver( CoreRequest request ) {
+        return new ProjectSaver( this, request );
+    }
+
+    public static class ProjectSaver extends Saver {
+
+        public ProjectSaver( AbstractNode modelObject, CoreRequest request ) {
+            super( modelObject, request );
+        }
+
+        @Override
+        public void save() throws SavingException {
+            super.save();
+
+            String description = request.getValue( "description", "" );
+            modelObject.setText( "description", description, request.getLanguage().getIdentifier() );
+        }
+    }
+
+    public String getDescription() {
+        return getText( "description", "" );
+    }
+
+    public static class ProjectDescriptor extends ResourceDescriptor<Project> {
+
+        @Override
+        public String getType() {
+            return "project";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Project";
+        }
+    }
 }
