626ce605afeb62cca38bdfef5c56c4fde542e849
Christian Wolfgang <wolfgarnet@gmail.com>
2014-02-12 23:32:40 +0100

    [PLUG-2] Added activity widget

--- core/src/main/java/org/seventyeight/web/widgets/ActivityWidget.java
+++ core/src/main/java/org/seventyeight/web/widgets/ActivityWidget.java
@@ -1,7 +1,13 @@
 package org.seventyeight.web.widgets;
 
+import org.seventyeight.database.mongodb.MongoDBCollection;
+import org.seventyeight.database.mongodb.MongoDBQuery;
+import org.seventyeight.database.mongodb.MongoDocument;
+import org.seventyeight.web.Core;
 import org.seventyeight.web.model.Node;
 import org.seventyeight.web.model.Widget;
+
+import java.util.List;
 
 /**
  * @author cwolfgang
@@ -26,5 +32,11 @@ public class ActivityWidget extends Widget {
     @Override
     public String getName() {
         return "Activity widget";
+    }
+
+    public List<MongoDocument> getActivities() {
+        MongoDocument sort = new MongoDocument().set( "updated", 1 );
+        MongoDBQuery query = new MongoDBQuery();
+        return MongoDBCollection.get( Core.RESOURCES_COLLECTION_NAME ).find( query, 0, 10, sort );
     }
 }
