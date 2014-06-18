4e8a34650d17d562ce3bb4aec60c7aa2db8839dc
Christian Wolfgang <wolfgarnet@gmail.com>
2014-06-18 22:19:53 +0200

    Added follow

---
+++ cms/src/main/java/org/seventyeight/web/social/Follow.java
@@ -0,0 +1,58 @@
+package org.seventyeight.web.social;
+
+import com.google.gson.JsonObject;
+import org.seventyeight.database.mongodb.MongoDocument;
+import org.seventyeight.web.extensions.ExtensionGroup;
+import org.seventyeight.web.extensions.NodeExtension;
+import org.seventyeight.web.model.AbstractExtension;
+import org.seventyeight.web.model.CoreRequest;
+import org.seventyeight.web.model.Node;
+import org.seventyeight.web.nodes.User;
+
+/**
+ * @author cwolfgang
+ */
+public class Follow extends NodeExtension<Follow> {
+
+    public Follow( Node parent, MongoDocument document ) {
+        super( parent, document );
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Follow";
+    }
+
+    @Override
+    public String getMainTemplate() {
+        return null;
+    }
+
+    @Override
+    public void updateNode( CoreRequest request, JsonObject jsonData ) {
+        /* Implementation is a no op */
+    }
+
+    public static class FollowDescriptor extends NodeExtensionDescriptor<Follow> {
+
+        @Override
+        public boolean isApplicable( Node node ) {
+            return node instanceof User;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Follow";
+        }
+
+        @Override
+        public String getExtensionName() {
+            return "follow";
+        }
+
+        @Override
+        public String getTypeName() {
+            return "follow";
+        }
+    }
+}
