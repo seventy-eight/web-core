da7488ac232d59644c8caae35a4ca03dc6c6e91d
Christian Wolfgang <wolfgarnet@gmail.com>
2014-10-20 23:40:06 +0200

    [CORE-106] JS, pt 2

--- cms/src/main/webapp/static/javascript/search.js
+++ cms/src/main/webapp/static/javascript/search.js
@@ -1,7 +1,23 @@
 
-function Search() {
+function Search(formId, queryInputId) {
 	this.query = "";
 	this.offset = 0;
+	
+	this.container = "#container";
+	this.moreId = '#more';
+	this.formId = formId;
+	
+	var THIS = this;
+	$(formId).submit(function(event) {
+		//debugger;
+		THIS.setQuery($(queryInputId).val());
+		THIS.fetchNext(10);
+		return false;
+	});
+	
+	$(this.moreId).click(function(event) {
+		THIS.fetchNext(10);
+	});
 }
 
 Search.prototype.setQuery = function(query) {
@@ -9,11 +25,12 @@ Search.prototype.setQuery = function(query) {
 }
 
 Search.prototype.fetchNext = function(number) {
+	var THIS = this;
     $.ajax({
         type: "GET",
         url: "search",
         data: { query: this.query, offset: this.offset, number: number },
-        success: function(data, textStatus, jqxhr){alert("YAY");},
+        success: function(data, textStatus, jqxhr){/*alert(JSON.stringify(data)) */;THIS.populate(eval(data));},
         error: function(ajax, text, error) {alert(error)}
     });
 
@@ -21,18 +38,20 @@ Search.prototype.fetchNext = function(number) {
 }
 
 Search.prototype.populate = function(jsonResult) {
+	//alert(jsonResult.length);
     if( jsonResult === undefined ) {
-        $('#bottom').hide();
+        $(this.container).hide();
     } else {
         //for( i = 0 ; i < jsonResult.length ; i++ ) {
     	for(var i in jsonResult) {
         	var data = jsonResult[i];
-            $('<tr><td id="node' + (i+offset) + '" style="text-align:center">' + data.document.avatar + '</td></tr>').hide().insertBefore("#bottom").fadeIn(600);
+        	//alert(i + ":" + data);
+            $('<div class="result" id="node' + (i+this.offset) + '" style="text-align:center">' + data.document.avatar + '</div>').hide().insertAfter(this.container).fadeIn(600);
             //getNode(data[i].document._id, "node" + (i+offset));
         }
 
         if( data.length < 10 ) {
-            $('#bottom').hide();
+            $(this.moreId).hide();
         }
     }
 }
